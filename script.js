fac8b98ab61ad0b84025791af79ab197ebb9ab32
George Nitu <george.nitu@netcompany.com>
2025-08-18 14:03:49 +0100

    Functioning drawing

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const container = document.querySelector("#draw_area");
+const drawableAreaSizeInPixels = 500
 
-for(let i = 0; i<64; i++) {
-    document.createElement("div")
-    container.append()
+let numberOfSquaresPerSide = 16
+let totalNumberOfSquares = Math.pow(numberOfSquaresPerSide,2)
+individualSquareArea = drawableAreaSizeInPixels/numberOfSquaresPerSide
+
+
+for(let i = 0; i<totalNumberOfSquares; i++) {
+    let paintableSquare = document.createElement("div")
+    paintableSquare.style.height = `${individualSquareArea}px`
+    paintableSquare.style.width  = `${individualSquareArea}px`
+    paintableSquare.style.boxSizing = "border-box"
+    paintableSquare.style.border = "solid blue 1px"
+
+    paintableSquare.addEventListener("mouseenter", (e) =>{
+        e.target.style.background = "black"
+    });
+
+
+    container.append(paintableSquare)
 }
