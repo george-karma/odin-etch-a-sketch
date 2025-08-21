cc6bbd926b5c9e4c30a4db14e61c9ec8df48b38c
Karma <22868369+george-karma@users.noreply.github.com>
2025-08-21 20:27:54 +0100

    Finalised Project

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 const container = document.querySelector("#draw_area");
+const slider = document.querySelector("#slider");
+const gridSizeLabel = document.querySelector("#gridSizeLabel");
 const drawableAreaSizeInPixels = 500
 
-let numberOfSquaresPerSide = 16
-let totalNumberOfSquares = Math.pow(numberOfSquaresPerSide,2)
-individualSquareArea = drawableAreaSizeInPixels/numberOfSquaresPerSide
+slider.addEventListener("input", (event) => {
+    let numberOfSquaresPerSide = event.target.value;
+    clearGrid()
+    drawGrid(numberOfSquaresPerSide)
+    updateGridLabel(numberOfSquaresPerSide)
+});
 
-
-for(let i = 0; i<totalNumberOfSquares; i++) {
-    let paintableSquare = document.createElement("div")
-    paintableSquare.style.height = `${individualSquareArea}px`
-    paintableSquare.style.width  = `${individualSquareArea}px`
-    paintableSquare.style.boxSizing = "border-box"
-    paintableSquare.style.border = "solid blue 1px"
-
-    paintableSquare.addEventListener("mouseenter", (e) =>{
-        e.target.style.background = "black"
-    });
-
-
-    container.append(paintableSquare)
+function clearGrid() {
+    container.innerHTML = "";
 }
+
+function updateGridLabel(numberOfSquaresPerSide){
+    gridSizeLabel.textContent = numberOfSquaresPerSide
+}
+
+function drawGrid(numberOfSquaresPerSide) {
+    let totalNumberOfSquares = Math.pow(numberOfSquaresPerSide, 2)
+    let individualSquareArea = drawableAreaSizeInPixels / numberOfSquaresPerSide
+
+    for (let i = 0; i < totalNumberOfSquares; i++) {
+        let paintableSquare = document.createElement("div")
+        paintableSquare.style.height = `${individualSquareArea}px`
+        paintableSquare.style.width = `${individualSquareArea}px`
+        paintableSquare.style.boxSizing = "border-box"
+
+        paintableSquare.addEventListener("mouseenter", (e) => {
+            e.target.style.background = "black"
+        });
+
+
+        container.append(paintableSquare)
+    }
+}
+
+drawGrid(16)
